da1fd270496692f218606f5542d921d8bb87c8cf
iamrobmat <robert.matyszewski@gmail.com>
2019-10-10 22:12:18 +0200

    feat:new styles

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const name = 'Robert'; 
 const age = 32;
 console.log(`Siemasz uzytkowniku! Nazywam się ${name} i mam ${age} lata`);
+const heading = document.querySelector('.about');
+console.log(heading);
